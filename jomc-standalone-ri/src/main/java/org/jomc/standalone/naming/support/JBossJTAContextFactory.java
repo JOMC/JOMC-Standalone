dfb0336a92d0936b0c440332ce61e371b5452ec1
Christian Schulte <cs@schulte.it>
2009-10-31 23:13:37 +0000

    [maven-scm-plugin] Build 2009-10-31T23:12:21+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@888 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-ri/src/main/java/org/jomc/standalone/naming/support/JBossJTAContextFactory.java
+++ jomc-standalone-ri/src/main/java/org/jomc/standalone/naming/support/JBossJTAContextFactory.java
@@ -69,8 +69,8 @@ import javax.sql.DataSource;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getImplementationInfoMessage implementationInfo}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-30T03:33:32+0000</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-30T03:33:32+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-31T23:12:21+0000</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-31T23:12:21+0000</pre></td></tr>
  * </table>
  * </ul></p>
  *
@@ -263,8 +263,8 @@ public class JBossJTAContextFactory extends AbstractContextFactory
     /**
      * Gets the text of the {@code implementationInfo} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-30T03:33:32+0000</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-30T03:33:32+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-31T23:12:21+0000</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>JBossJTAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-10-31T23:12:21+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code implementationInfo} message.
