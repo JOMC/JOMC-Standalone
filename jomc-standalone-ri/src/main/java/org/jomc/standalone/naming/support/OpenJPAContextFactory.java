dc2dc369b953da6837b39283adc39bee07050404
Christian Schulte <cs@schulte.it>
2009-11-07 23:06:37 +0000

    [maven-scm-plugin] Build 2009-11-07T23:06:00+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@922 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-ri/src/main/java/org/jomc/standalone/naming/support/OpenJPAContextFactory.java
+++ jomc-standalone-ri/src/main/java/org/jomc/standalone/naming/support/OpenJPAContextFactory.java
@@ -51,8 +51,8 @@ import org.apache.openjpa.persistence.PersistenceProviderImpl;
  * </ul></p>
  * <p><b>Messages</b><ul>
  * <li>"{@link #getImplementationInfoMessage implementationInfo}"<table>
- * <tr><td valign="top">English:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-04T16:05:41+0000</pre></td></tr>
- * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-04T16:05:41+0000</pre></td></tr>
+ * <tr><td valign="top">English:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-07T23:06:00+0000</pre></td></tr>
+ * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-07T23:06:00+0000</pre></td></tr>
  * </table>
  * </ul></p>
  *
@@ -133,8 +133,8 @@ public class OpenJPAContextFactory extends AbstractJPAContextFactory
     /**
      * Gets the text of the {@code implementationInfo} message.
      * <p><b>Templates</b><br/><table>
-     * <tr><td valign="top">English:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-04T16:05:41+0000</pre></td></tr>
-     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-04T16:05:41+0000</pre></td></tr>
+     * <tr><td valign="top">English:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-07T23:06:00+0000</pre></td></tr>
+     * <tr><td valign="top">Deutsch:</td><td valign="top"><pre>OpenJPAContextFactory Version 1.0-alpha-3-SNAPSHOT Build 2009-11-07T23:06:00+0000</pre></td></tr>
      * </table></p>
      * @param locale The locale of the message to return.
      * @return The text of the {@code implementationInfo} message.
