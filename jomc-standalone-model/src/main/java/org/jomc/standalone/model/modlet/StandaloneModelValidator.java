ec0be224a556dd271dffdb0920ad01d2dfa5ef47
Christian Schulte <cs@schulte.it>
2010-06-24 11:03:01 +0000

    [maven-scm-plugin] Build 2010-06-24T13:02:26+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@2113 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
+++ jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
@@ -64,8 +64,7 @@ import org.jomc.standalone.model.MethodsType;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-5-SNAPSHOT", comments =
-"See http://jomc.sourceforge.net/jomc/1.0-beta-5-SNAPSHOT/jomc-tools" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-5-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-5-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public final class StandaloneModelValidator implements ModelValidator
@@ -157,9 +156,9 @@ public final class StandaloneModelValidator implements ModelValidator
     // SECTION-END
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
+
     /** Creates a new {@code StandaloneModelValidator} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-5-SNAPSHOT", comments =
-    "See http://jomc.sourceforge.net/jomc/1.0-beta-5-SNAPSHOT/jomc-tools" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-5-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-5-SNAPSHOT/jomc-tools" )
     public StandaloneModelValidator()
     {
         // SECTION-START[Default Constructor]
