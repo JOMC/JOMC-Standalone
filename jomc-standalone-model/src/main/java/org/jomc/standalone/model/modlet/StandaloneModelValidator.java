11fdef2870de20c61e640a56aab7d71bdfa196d2
Christian Schulte <cs@schulte.it>
2011-01-24 19:29:00 +0000

    [maven-scm-plugin] Build 2011-01-24T20:23:55+0100
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@2993 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
+++ jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
@@ -1,8 +1,8 @@
 // SECTION-START[License Header]
 // <editor-fold defaultstate="collapsed" desc=" Generated License ">
 /*
- *   Copyright (c) 2010 The JOMC Project
- *   Copyright (c) 2005 Christian Schulte <schulte2005@users.sourceforge.net>
+ *   Copyright (c) 2009 - 2011 The JOMC Project
+ *   Copyright (c) 2005 - 2011 Christian Schulte <schulte2005@users.sourceforge.net>
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -65,7 +65,7 @@ import org.jomc.standalone.model.MethodsType;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1", comments = "See http://jomc.sourceforge.net/jomc/1.1/jomc-tools-1.1" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.2-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-1.2-SNAPSHOT" )
 // </editor-fold>
 // SECTION-END
 public final class StandaloneModelValidator implements ModelValidator
@@ -154,7 +154,7 @@ public final class StandaloneModelValidator implements ModelValidator
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
 
     /** Creates a new {@code StandaloneModelValidator} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.1", comments = "See http://jomc.sourceforge.net/jomc/1.1/jomc-tools-1.1" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.2-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-1.2-SNAPSHOT" )
     public StandaloneModelValidator()
     {
         // SECTION-START[Default Constructor]
