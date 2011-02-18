924a0e762bc9b2f30eb73ac3cf03eb577ebb44b6
Christian Schulte <cs@schulte.it>
2011-02-18 00:45:21 +0000

    o s/(c)/(C)/g
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@3147 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-ri/src/main/java/org/jomc/standalone/ri/naming/support/XAPoolHelper.java
+++ jomc-standalone-ri/src/main/java/org/jomc/standalone/ri/naming/support/XAPoolHelper.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2009 The JOMC Project. All rights reserved.
+ *   Copyright (C) 2009 The JOMC Project. All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
