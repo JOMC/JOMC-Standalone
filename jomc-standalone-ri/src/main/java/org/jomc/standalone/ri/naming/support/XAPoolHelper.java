e2c18b87f2057364574d4c2073054ae536b2a1a8
Christian Schulte <cs@schulte.it>
2011-10-01 22:18:35 +0000

    o Removed 'The JOMC Project' copyright.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@3801 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-ri/src/main/java/org/jomc/standalone/ri/naming/support/XAPoolHelper.java
+++ jomc-standalone-ri/src/main/java/org/jomc/standalone/ri/naming/support/XAPoolHelper.java
@@ -1,5 +1,4 @@
 /*
- *   Copyright (C) 2009 The JOMC Project. All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
