e6321c544da80199cd4b7337083554a41f1657d7
Christian Schulte <cs@schulte.it>
2011-10-24 11:37:28 +0000

    [maven-scm-plugin] Build 2011-10-24T11:34:01+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-standalone/trunk@3903 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
+++ jomc-standalone-model/src/main/java/org/jomc/standalone/model/modlet/StandaloneModelValidator.java
@@ -155,7 +155,6 @@ public final class StandaloneModelValidator implements ModelValidator
     // SECTION-END
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
-
     /** Creates a new {@code StandaloneModelValidator} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.2-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-1.2-SNAPSHOT" )
     public StandaloneModelValidator()
